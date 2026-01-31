1ab50cbf160ee645e3e1856c1e322bd3076d674c
vjimgue <101817874+vjimgue@users.noreply.github.com>
2026-01-31 12:06:08 +0100

    Moved dockerfiles

--- Dockerfiles/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Start Jupyter Notebook in the background
-jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root &
-
-# Start Streamlit in the foreground
-# (The container stays alive as long as this process runs)
-streamlit run streamlit_app.py --server.port=8501 --server.address=0.0.0.0
