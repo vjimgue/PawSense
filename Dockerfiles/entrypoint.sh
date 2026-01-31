842029e304767346ef8e3ef0f4b8afe1369f2c6a
Enrique Moreno <enrimail8@gmail.com>
2026-01-31 14:54:10 +0100

    Opcion docker sin gpu (Enrique) + organización de cuadernos.

--- Dockerfiles/entrypoint.sh
+++ Dockerfiles/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
+# Enrique
+# Limpiamos posibles procesos previos
+# pkill -f jupyter
+# pkill -f streamlit
+
 # Start Jupyter Notebook in the background
 jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root &
+
+# Enrique: sleep 5
 
 # Start Streamlit in the foreground
 # (The container stays alive as long as this process runs)
